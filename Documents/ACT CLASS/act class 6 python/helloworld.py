8fbe9c8f6ba596ba1d5a0addb0f01221ddceb64d
zerihun-cs <zerihun.cs@gmail.com>
2022-07-03 10:41:59 +0300

    java array example

---
+++ Documents/ACT CLASS/act class 6 python/helloworld.py
@@ -0,0 +1,10 @@
+# Python is a stunning programming language.
+# It's simple to pick up and play, with clear and short grammar (rules).
+# Python is a popular choice for novices, but it's also strong enough to run some of the world's
+# most popular products and services, including those from NASA, Google, IBM, Cisco, Microsoft, and Industrial Light & Magic.
+
+
+
+# The Python print() function takes in any number of parameters, and prints them out on one line of text.
+# The items are each converted to text form, separated by spaces, and there is a single '\n' at the end (the "newline" char).
+print("Hello, world!") 
